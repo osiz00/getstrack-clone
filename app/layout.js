a8a64990197770344bfd5e27384d47fc2254df36
Ahmed ezaldean <ahmedezealdean0125@gmail.com>
2023-09-08 21:54:18 +0300

    remove-inter

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,4 @@
-import Head from 'next/head'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'getstrack',
@@ -15,7 +11,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel='icon' href='/images/logo.svg' />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
